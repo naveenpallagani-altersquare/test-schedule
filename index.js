3a715b0fcb67cd825339a3322ef677151930b25c
naveenpallagani-altersquare <naveen.pallagani@altersquare.io>
2026-07-18 14:12:59 +0530

    Refactor index.js to use destructured import for child_process

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ for (let i = 0; i < 5; i++) {
   console.log(i);
 }
 
-const { exec } = require("child_process");
+const child_process = require("child_process");
 const userInput = process.argv[2];
-exec("ls " + userInput);
+child_process.exec("ls " + userInput);
